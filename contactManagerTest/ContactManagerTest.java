ad16d63261000ca5d65cb238ba7bfa8e9d327e18
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-03-09 19:12:58 +0000

    Way better to start with the contact, as it's going to be needed to test the ContactManager

--- contactManagerTest/ContactManagerTest.java
+++ contactManagerTest/ContactManagerTest.java
@@ -2,13 +2,13 @@ package contactManagerTest;
 
 import org.junit.*;
 
-import contactManagerInterfaces.ContactManager;
+import contactManagerInterfaces.*;
 
 public class ContactManagerTest {
 	
 	@Before
 	public void instantiateClass() {
-		ContactManager testManager = new DummyContactManagerImpl();
+		Contact testContact = new DummyContactImpl();
 	}
 	
 }
---
+++ contactManagerTest/DummyContactImpl.java
@@ -0,0 +1,30 @@
+package contactManagerTest;
+import contactManagerInterfaces.Contact;
+
+public class DummyContactImpl implements Contact {
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNotes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addNotes(String note) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
