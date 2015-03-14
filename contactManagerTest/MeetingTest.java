07e204b73d2717058439712fed0538ce2e048786
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-03-14 19:36:59 +0000

    The getID should return the same ID that was passed in the act of instantiation

--- contactManagerTest/MeetingTest.java
+++ contactManagerTest/MeetingTest.java
@@ -6,16 +6,23 @@ import contactManagerInterfaces.Meeting;
 
 public class MeetingTest {
 	
-	Meeting testMeeting = new DummyMeetingImpl();
+	private Meeting testMeeting;
+	
+	private int testID = 1;
 	
 	@Before
 	public void instantiateClass() {
+		testMeeting = new DummyMeetingImpl(testID);
 	}
-	
 	
 	@Test
 	public final void getIdShouldBeGreaterThanZero() {
 		Assert.assertTrue(testMeeting.getId() > 0);
+	}
+	
+	@Test
+	public final void getIdShouldgiveUsTheSameIDWhenWeInstantiated() {
+		Assert.assertTrue(testMeeting.getId(), testID);
 	}
 
 }
