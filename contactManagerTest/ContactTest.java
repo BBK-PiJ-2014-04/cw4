404f0204d8852e8d07b1dd155af4c6b56d8e4f6b
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-03-15 13:24:09 +0000

    Writing the test for the Contact Class, plus the dummy Contact Class. (I was sure I've written that already from the Lab Computer but apparently something went lost during the commit/push)

---
+++ contactManagerTest/ContactTest.java
@@ -0,0 +1,29 @@
+package contactManagerTest;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import contactManagerInterfaces.Contact;
+
+public class ContactTest {
+	
+	private Contact testContact;
+	private int testID;
+	
+	@Before
+	public void instantiateClass() {
+		testContact = new DummyContactImpl();
+	}
+	
+	@Test
+	public final void getIdShouldBeGreaterThanZero() {
+		Assert.assertTrue(testContact.getId() > 0);
+	}
+	
+	@Test
+	public final void getIdShouldReturnTheSameIDofWhatItWasWhenInitiated() {
+		Assert.assertEquals(testContact.getId(), testID);
+	}
+	
+}
