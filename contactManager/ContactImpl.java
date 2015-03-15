0b130b36ce5bfd7079de5ebbcd1acee048f926ca
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-03-15 18:13:51 +0000

    Checking in the actual ContactImpl class

---
+++ contactManager/ContactImpl.java
@@ -0,0 +1,44 @@
+package contactManager;
+
+import contactManagerInterfaces.Contact;
+
+public class ContactImpl implements Contact {
+	
+	private int id;
+	private String name;
+	private String notes;
+	
+	public ContactImpl(int id, String name) {
+		this.id = id;
+		this.notes = "";
+		if(name != "") {
+			this.name = name;
+		}
+		else {
+			throw new IllegalArgumentException("The name of the Contact can't be null");
+		}
+	}
+	@Override
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String getNotes() {
+		return notes;
+	}
+
+	@Override
+	public void addNotes(String note) {
+		if(note == null) {
+			throw new IllegalArgumentException("Null is an invalid parameter for addNotes");
+		}
+		this.notes = note;
+	}
+
+}
