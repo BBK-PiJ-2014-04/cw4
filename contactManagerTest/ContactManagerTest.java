50daf7bf668387eb8891f5f215bbec20740d5c7f
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-03-21 19:44:12 +0000

    Setting up the test for the contactManager, the main part

--- contactManagerTest/ContactManagerTest.java
+++ contactManagerTest/ContactManagerTest.java
@@ -1,26 +1,27 @@
 package contactManagerTest;
 
-/*import org.junit.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
-import contactManagerInterfaces.*;*/
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import contactManagerInterfaces.*;
 
 public class ContactManagerTest {
-	/*
-	Contact testContact = new DummyContactImpl();
+	
+	Contact testContact;
+	Calendar pastdate;
+	Calendar futuredate;
 	
 	
 	@Before
 	public void instantiateClass() {
+		pastdate = new GregorianCalendar(1986,07,27);
+		futuredate = new GregorianCalendar(1986,07,27);
 	}
 	
-	@Test
-	public final void getIdShouldBeGreaterThanZero() {
-		Assert.assertTrue(testContact.getId() > 0);
-	}
 	
-	@Test(expected = RuntimeException.class)
-	public final void instantiatenewContactClass(){
-		ContactImpl testContactImpl = new ContactImpl();
-	}*/
 	
 }
