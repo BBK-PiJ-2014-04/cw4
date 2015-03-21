cd024b5ce72a5f095c97dfbcd057389b3c0bd9e7
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-03-21 12:42:54 +0000

    Class Meeting implemented

--- contactManager/MeetingImpl.java
+++ contactManager/MeetingImpl.java
@@ -3,24 +3,40 @@ package contactManager;
 import java.util.Calendar;
 import java.util.Set;
 
+import contactManagerInterfaces.Contact;
 import contactManagerInterfaces.Meeting;
 
-public class MeetingImpl {
+public class MeetingImpl implements Meeting {
 	
-	public int id;
+	private int id;
 	private Calendar meetingDate;
+	private Set<Contact> attendingContacs;
+	
 
-	public MeetingImpl(int id, Calendar meetingDate) {
+	public MeetingImpl(int id, Calendar meetingDate, Set<Contact> attendingContacs) {
 		this.id = id;
 		this.meetingDate = meetingDate;
+		if(attendingContacs.size() > 0) {
+			this.attendingContacs = attendingContacs;
+		}
+		else {
+			throw new IllegalArgumentException("The name of the Contact can't be null");
+		}
 	}
 	
+	@Override
 	public int getId() {
 		return id;
 	}
-	
+
+	@Override
 	public Calendar getDate() {
 		return meetingDate;
+	}
+
+	@Override
+	public Set<Contact> getContacts() {
+		return attendingContacs;
 	}
 
 
