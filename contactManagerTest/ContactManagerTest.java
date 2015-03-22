05bf65cea3e07467ca8c2cf9618a8ee60ae80b13
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-03-22 23:08:46 +0000

    Three tests that should cover the all addNewContact Method

--- contactManagerTest/ContactManagerTest.java
+++ contactManagerTest/ContactManagerTest.java
@@ -3,6 +3,7 @@ package contactManagerTest;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import org.junit.Assert;
@@ -28,10 +29,6 @@ public class ContactManagerTest {
 		pastDate = new GregorianCalendar(1986,07,27);
 		futureDate = new GregorianCalendar(2015,07,27);
 		testCM = new DummyContactManagerImpl();
-		Contact testContact1 = new ContactImpl(1,"FirstContact");
-		Contact testContact2 = new ContactImpl(2,"FirstContact2");
-		testContacts.add(testContact1);
-		testContacts.add(testContact2);
 	}
 	
 	//The following tests will require very little implementation, as they've been already handled in the implementation of the Meeting Class
@@ -42,6 +39,10 @@ public class ContactManagerTest {
 	
 	@Test(expected = IllegalArgumentException.class)
 	public final void addFutureMeetingShouldNotAcceptAPastDate() {
+		Contact testContact1 = new ContactImpl(1,"FirstContact");
+		Contact testContact2 = new ContactImpl(2,"FirstContact2");
+		testContacts.add(testContact1);
+		testContacts.add(testContact2);
 		testCM.addFutureMeeting(testContacts, pastDate);
 	}
 	
@@ -70,6 +71,48 @@ public class ContactManagerTest {
 	public final void getContactsShouldReturnASet() {
 		String name = "test";
 		Assert.assertTrue(testCM.getContacts(name) instanceof Set<?>);
+	}
+	
+	//Again, mostly of the functionality for addNewContact have already been tested for the Contact Class
+	//So, the tests, will succeed with a minimum implementation.
+	@Test(expected = NullPointerException.class)
+	public final void addNewContactShouldNotAcceptNullForName() {
+		String name = null;
+		String notes = "something";
+		testCM.addNewContact(name, notes);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public final void addNewContactShouldNotAcceptNullForNotes() {
+		String name = "something";
+		String notes = null;
+		testCM.addNewContact(name, notes);
+	}
+	
+	@Test
+	public final void addNewContactShouldAddTheContactOnTheContacsList() {
+		String name = "something";
+		String notes = "something";
+		boolean alreadyIn = false;
+		boolean nowIn = false;
+		//Making sure the name is not already in
+		do {
+			for(Iterator<Contact> i = testContacts.iterator(); i.hasNext(); ) {
+				Contact item = (Contact) i.next();
+			    if(item.getName().equals(name))
+			    	alreadyIn = true;
+			}
+			if(alreadyIn){
+				name += "1";
+			}
+		} while(alreadyIn);
+		testCM.addNewContact(name, notes);
+		for(Iterator<Contact> i = testContacts.iterator(); i.hasNext(); ) {
+			Contact item = (Contact) i.next();
+		    if(item.getName().equals(name))
+		    	nowIn = true;
+		}
+		Assert.assertTrue(nowIn);
 	}
 	
 }
